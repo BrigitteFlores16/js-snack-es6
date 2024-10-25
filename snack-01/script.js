5ccdf3a600fc9ed3c60867da1233067390835643
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-10-25 17:22:06 +0200

    completato es.1,2

--- snack-01/script.js
+++ snack-01/script.js
@@ -1,5 +1,24 @@
-//*Snack 1
+//Snack 1
 //Creare un array di oggetti:
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
+const biciclette = [
+    { nome: "Triban RC120", peso: 6.8 },
+    { nome: "Van Rysel RCR", peso: 7.0 },
+    { nome: "Roubaix SL8 ", peso: 6.4 },
+    { nome: "Works Venge", peso: 6.6 }
+];
+
+// la bici con il peso minore
+let biciLeggera = biciclette[0]; 
+
+for (let i = 1; i < biciclette.length; i++) {
+    if (biciclette[i].peso < biciLeggera.peso) {
+        biciLeggera = biciclette[i];
+    }
+}
+     const biciLeggeraDiv = document.getElementById('biciLeggera');
+        biciLeggeraDiv.textContent = `${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg`;
+
+console.log("La bici più leggera è:", biciLeggera.nome, "con un peso di", biciLeggera.peso, "kg");
 
--- snack-02/script.js
+++ snack-02/script.js
@@ -1,6 +1,30 @@
-//*Snack 2
+//Snack 2
 //Creare un array di oggetti di squadre di calcio.
 //Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 //Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// le squadre di calcio 
+const squadre = [
+    { nome:"Inter", punti: 0, falliSubiti: 0},
+    { nome:"Juventus", punti: 0, falliSubiti: 0},
+    { nome:"Milan", punti: 0, falliSubiti: 0}, 
+    { nome:"Roma", punti: 0, falliSubiti: 0},
+];
+
+ for (let i = 0; i < squadre.length; i++) {
+    squadre[i].punti = getRandomInt(0, 100); 
+    squadre[i].falliSubiti = getRandomInt(0, 50);
+}
+
+ const squadreFalli = squadre.map(squadra => {
+    return { nome: squadra.nome, falliSubiti: squadra.falliSubiti };
+});
+
+console.log("Squadre con tutte le proprietà:", squadre);
+console.log("Nuovo array con nomi e falli subiti:", squadreFalli);
+
